7cfe3f8011e8cab89c6f406b43831e322ae4e1a7
An Duong <anqduong2003@gmail.com>
2022-05-15 22:52:08 -0500

    create database frame

--- server/server.js
+++ server/server.js
@@ -29,15 +29,32 @@ app.get('/api/v1/users/:id', (req, res) => {
 //Create user
 app.post('/api/v1/users', (req, res) => {
     console.log(req.body);
+    res.status(201).json({
+        status: 'success',
+        data: {
+            user: 'adonk'
+        }
+    });
 });
 
 //Update user
 app.put('/api/v1/users/:id', (req, res) => {
     console.log(req.params.id);
     console.log(req.body);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: 'adonk'
+        }
+    });
 });
 
-
+//delete user
+app.delete('/api/v1/users/:id', (req , res) => {
+    res.status(204).json({
+        status: 'success'
+    });
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {

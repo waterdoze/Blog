0d3668e08e9e1fec7f7a263fe582fd358552b0bb
An Duong <anqduong2003@gmail.com>
2022-05-20 00:02:22 -0500

    did stuff for login page

---
+++ client/src/context/LoginContext.js
@@ -0,0 +1,13 @@
+import React, {useState, createContext} from "react";
+
+export const LoginContext = createContext();
+
+export const LoginContextProvider = props => {
+
+    const [users, setUsers] = useState([])
+    return (
+        <LoginContext.Provider value={{users, setUsers}}>
+            {props.children}
+        </LoginContext.Provider>
+    )
+}
--- client/src/home_components/Login.jsx
+++ client/src/home_components/Login.jsx
@@ -3,7 +3,29 @@ import React from "react";
 const Login = () => {
     return (
         <div>
-            <h1 className='text-center display-1'>Reddle</h1>
+            <div>
+                <div class='display-1 text-center'>Reddle</div>
+            </div>
+
+            <form class="shadow w-25 m-auto mt-5 p-3 border">
+                <div class="form-group mb-3">
+                    <label for="inputUsername">Username</label>
+                    <input type="username" class="form-control" id="inputEmail" placeholder="Enter Username"/>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="Password"/>
+                </div>
+
+                <button type="register" class="btn btn-link mb-3">No Account? Register Here</button>
+
+                <div class="text-center">
+                    
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+                
+
+            </form>
         </div>
 
     )
